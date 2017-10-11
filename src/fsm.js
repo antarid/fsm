a38e256a4a20929a76460e72ced02dc84e810198
antarid <tarand007@gmail.com>
2017-10-11 13:29:43 +0300

    12/25

--- src/fsm.js
+++ src/fsm.js
@@ -7,8 +7,10 @@ class FSM {
         this.config = config;
         this.currentState = config.initial;
         this.states = config.states;
+        this.array = [];
+        this.array.push(this.currentState);
     }
-
+  
     /**
      * Returns active state.
      * @returns {String}
@@ -16,29 +18,35 @@ class FSM {
     getState() {
         return this.currentState;
     }
-
+  
     /**
      * Goes to specified state.
      * @param state
      */
     changeState(state) {
-        if(state in this.states)
+        if(state in this.states){
             this.currentState = state;
+            this.array.push(this.currentState);
+        }
         else
-            throw "ERROR";
+            new Error();
            
     }
-
+  
     /**
      * Changes state according to event transition rules.
      * @param event
      */
     trigger(event) {
-        if(event in this.currentState[transitions]){
-            this.currentState = this.currentState[transitions][event];
+        if(event in this.config.states[this.currentState].transitions){
+            this.currentState = this.config.states[this.currentState].transitions[event];
+            this.array.push(this.currentState);
+        }
+        else{
+          new Error();
         }
     }
-
+  
     /**
      * Resets FSM state to initial.
      */
@@ -46,34 +54,56 @@ class FSM {
     reset() {
         this.currentState = this.config.initial;
     }
-
+  
     /**
      * Returns an array of states for which there are specified event transition rules.
      * Returns all states if argument is undefined.
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
-
+    
+    getStates(event) {
+        
+  
+        if(!event){
+          var result = [];
+          for(var key in this.config.states)
+            result.push(key);
+          return result;
+        }
+        else
+            return this.array;
+    }
+  
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
-
+    undo() {
+        if(this.array.length == 0 || this.array.length == 1)
+            return false;
+        if(this.array[this.array.length - 2]){
+            this.currentState = this.array[this.array.length - 2];
+            return true;
+        }
+        return false;
+    }
+  
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
-     */
+     */ 
     redo() {}
-
+  
     /**
      * Clears transition history
      */
-    clearHistory() {}
-}
+    clearHistory() {
+        this.array = [];
+    }
+  }
 
 module.exports = FSM;
 
